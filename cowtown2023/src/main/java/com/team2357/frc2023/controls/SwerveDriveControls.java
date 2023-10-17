83e503a2f600bc759812598ce897682fce6cf9f8
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2023-10-17 13:28:25 -0700

    add test controls

--- cowtown2023/src/main/java/com/team2357/frc2023/controls/SwerveDriveControls.java
+++ cowtown2023/src/main/java/com/team2357/frc2023/controls/SwerveDriveControls.java
@@ -1,36 +1,46 @@
 package com.team2357.frc2023.controls;
 
 import com.team2357.frc2023.commands.intakeRoller.IntakeRollerEjectCubeCommand;
+import com.team2357.frc2023.commands.intakeRoller.IntakeRollerIndexCubeCommand;
 import com.team2357.frc2023.commands.intakeRoller.IntakeRollerPickupCubeCommand;
+import com.team2357.frc2023.commands.intakeRoller.IntakeRollerRollCubeCommand;
 import com.team2357.lib.triggers.AxisThresholdTrigger;
+import com.team2357.lib.util.XboxRaw;
 
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class SwerveDriveControls implements RumbleInterface {
 
     private XboxController m_controller;
     private double m_deadband;
 
-    public static boolean isFlipped;
-
     private AxisThresholdTrigger m_rightTrigger;
     private AxisThresholdTrigger m_leftTrigger;
+    private JoystickButton m_rightBumper;
+    private JoystickButton m_leftBumper;
+
+    public static boolean isFlipped;
 
     public SwerveDriveControls(int portNumber, double deadband) {
         m_controller = new XboxController(portNumber);
         m_deadband = deadband;
-        
+
         m_rightTrigger = new AxisThresholdTrigger(m_controller, Axis.kRightTrigger, 0.0);
         m_leftTrigger = new AxisThresholdTrigger(m_controller, Axis.kLeftTrigger, 0.0);
-        
+        m_rightBumper = new JoystickButton(m_controller, XboxRaw.BumperRight.value);
+        m_leftBumper = new JoystickButton(m_controller, XboxRaw.BumperLeft.value);
+
         mapControls();
     }
 
     private void mapControls() {
         m_rightTrigger.whileTrue(new IntakeRollerPickupCubeCommand());
         m_leftTrigger.whileTrue(new IntakeRollerEjectCubeCommand());
+        m_rightBumper.whileTrue(new IntakeRollerRollCubeCommand());
+        m_leftBumper.whileTrue(new IntakeRollerIndexCubeCommand());
     }
 
     public double getX() {
