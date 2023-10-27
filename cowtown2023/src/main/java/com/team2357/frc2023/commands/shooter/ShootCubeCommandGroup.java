985ecf09d35c53aed99b71e7d6fc15c519cd7d39
coderkevin <kevin@redfenix.com>
2023-10-27 10:57:28 -0500

    Add SmartDashboard properties for shooter tuning

--- cowtown2023/src/main/java/com/team2357/frc2023/commands/shooter/ShootCubeCommandGroup.java
+++ cowtown2023/src/main/java/com/team2357/frc2023/commands/shooter/ShootCubeCommandGroup.java
@@ -1,38 +1,66 @@
 package com.team2357.frc2023.commands.shooter;
 
 import com.team2357.frc2023.Constants;
-import com.team2357.frc2023.Robot;
 import com.team2357.frc2023.commands.intakeRoller.IntakeRollerIndexCubeCommand;
 
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class ShootCubeCommandGroup extends SequentialCommandGroup {
     
     public enum SHOOTER_RPMS {
-        LOW(Constants.SHOOTER.TOP_MOTOR_LOW_RPMS, Constants.SHOOTER.BOTTOM_MOTOR_LOW_RPMS),
-        MID(Constants.SHOOTER.TOP_MOTOR_MID_RPMS, Constants.SHOOTER.BOTTOM_MOTOR_MID_RPMS),
-        HIGH(Constants.SHOOTER.TOP_MOTOR_HIGH_RPMS, Constants.SHOOTER.BOTTOM_MOTOR_HIGH_RPMS),
-        FAR(Constants.SHOOTER.TOP_MOTOR_FAR_RPMS, Constants.SHOOTER.BOTTOM_MOTOR_FAR_RPMS);
+        LOW(
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_LOW_TOP,
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_LOW_BOTTOM,
+            Constants.SHOOTER.TOP_MOTOR_LOW_RPMS,
+            Constants.SHOOTER.BOTTOM_MOTOR_LOW_RPMS
+        ),
+        MID(
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_MID_TOP,
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_MID_BOTTOM,
+            Constants.SHOOTER.TOP_MOTOR_MID_RPMS,
+            Constants.SHOOTER.BOTTOM_MOTOR_MID_RPMS
+        ),
+        HIGH(
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_HIGH_TOP,
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_HIGH_BOTTOM,
+            Constants.SHOOTER.TOP_MOTOR_HIGH_RPMS,
+            Constants.SHOOTER.BOTTOM_MOTOR_HIGH_RPMS
+        ),
+        FAR(
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_FAR_TOP,
+            Constants.SHOOTER.SMART_DASHBOARD_SHOOT_FAR_BOTTOM,
+            Constants.SHOOTER.TOP_MOTOR_FAR_RPMS,
+            Constants.SHOOTER.BOTTOM_MOTOR_FAR_RPMS
+        );
 
-        public double top, bottom;
+        private double m_topDefaultRpms;
+        private double m_bottomDefaultRpms;
+        private String m_topRpmsName;
+        private String m_bottomRpmsName;
 
-        SHOOTER_RPMS(double topRPMs, double bottomRPMs) {
-            this.top = topRPMs;
-            this.bottom = bottomRPMs;
+        SHOOTER_RPMS(String topRpmsName, String bottomRpmsName, double topDefaultRpms, double bottomDefaultRpms) {
+            this.m_topRpmsName = topRpmsName;
+            this.m_topDefaultRpms = topDefaultRpms;
+            this.m_bottomRpmsName = bottomRpmsName;
+            this.m_bottomDefaultRpms = bottomDefaultRpms;
+            SmartDashboard.putNumber(m_topRpmsName, m_topDefaultRpms);
+            SmartDashboard.putNumber(m_bottomRpmsName, m_bottomDefaultRpms);
+        }
+
+        public double getTopRpms() {
+            return SmartDashboard.getNumber(m_topRpmsName, m_topDefaultRpms);
+        }
+
+        public double getBottomRpms() {
+            return SmartDashboard.getNumber(m_bottomRpmsName, m_bottomDefaultRpms);
         }
     }
 
-    private SHOOTER_RPMS m_rpms;
-
-    public ShootCubeCommandGroup(SHOOTER_RPMS rpms) {
+    public ShootCubeCommandGroup(SHOOTER_RPMS position) {
         super(
-            new ShooterSetRPMsCommand(rpms.top, rpms.bottom), 
+            new ShooterSetRPMsCommand(position.getTopRpms(), position.getBottomRpms()), 
             new IntakeRollerIndexCubeCommand()
         );
     }
-
-
-
 }
